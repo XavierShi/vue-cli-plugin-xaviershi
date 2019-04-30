c93e71b757db3fda1540ac932c927f87ffc8c894
XavierShi <shiguoqiang183@gmail.com>
2019-04-30 20:33:34 +0800

    Version 0.06

--- generator.js
+++ generator.js
@@ -1,15 +1,29 @@
 module.exports = (api, options, rootOptions) => {
     // 修改 `package.json` 里的字段
     api.extendPackage({
-        scripts: {
-            test: 'vue-cli-service test'
-        },
         dependencies: {
-            axios: '^0.18.0'
-        }
+            'animate.css': "^3.7.0",
+            'axios': '^0.18.0',
+            'dayjs': '^1.8.13',
+            'number-precision': '^1.3.1',
+            'tween.js': '^16.6.0',
+            'vue-i18n': '^8.11.2'
+        },
+        devDependencies: {
+            'node-sass': '^4.12.0',
+            'sass-loader': '^7.1.0',
+            'less': '^3.9.0',
+            'less-loader': '^5.0.0',
+        },
+        browserslist: [
+            "> 1%",
+            "last 2 versions",
+            "not ie <= 8"
+        ]
     })
 
     // 复制并用 ejs 渲染 `./template` 内所有的文件
+    api.render('./template')
 
     if (options.foo) {
         // 有条件地生成文件
